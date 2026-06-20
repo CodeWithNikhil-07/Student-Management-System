840635f919126b716a2d58966752c590798eba23
Nikhil Das <nikhil.das6986@gmail.com>
2026-06-20 23:15:14 +0530

    Implemented CRUD operations

--- app.js
+++ app.js
@@ -6,11 +6,36 @@ const port = process.env.PORT;
 const mongoose = require("mongoose");
 const connectDb = require("./db/db");
 const studentModel = require("./models/student");
+const path = require("path");
 
 connectDb();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/",(req,res) => {
     res.send("Hello");
+})
+
+app.post("/students", async(req,res) => {
+    const student = new studentModel(req.body);
+    await student.save();
+
+    res.redirect("/students");
+});
+
+app.get("/students/:id", async(req,res) => {
+    let {id} = req.params;
+
+    let getData = await studentModel.findById(id);
+    res.send(getData);
+})
+
+app.get("/students/:id/edit", async(req,res) => {
+    let {id} = req.params;
+    let student = await studentModel.findById(id);
+
+    res.render("students/edit",{student});
 })
 
 app.listen(port, () => {
